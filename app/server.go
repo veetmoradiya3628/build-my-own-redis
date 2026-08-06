c490b17016d5a28469cb6854129a6c060f28e879
veet <veetmoradiya3628@gmail.com>
2026-08-06 19:23:17 +0000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -61,6 +61,20 @@ func main() {
 		initialExpiry = make(map[string]time.Time)
 	}
 
+	// 2. Check if appendonly is enabled create the <dir>/<appenddirname> directory if it doesn't exist
+	if config.appendonly == "yes" {
+		aofPath := filepath.Join(config.appenddirname, config.appendfilename)
+		slog.Info("Append-only mode enabled", "aof_path", aofPath)
+		
+		if err := os.MkdirAll(config.appenddirname, 0755); err != nil {
+			slog.Error("failed to create append-only directory", "err", err)
+			os.Exit(1)
+		}
+		slog.Info("Append-only directory ensured", "path", config.appenddirname)
+	} else {
+		slog.Info("Append-only mode disabled")
+	}
+
 	// Update NewStore call
 	store := NewStore(initialData, initialExpiry)
 
